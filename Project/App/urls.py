68df5f909f31586e69ef7bd025a75f4171a0e8f0
TekyUser <toantekyservice@gmail.com>
2024-10-12 08:33:15 +0000

    Home Page Adjust the gallery

--- Project/App/urls.py
+++ Project/App/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
-    path('/gallery', views.gallery_page, name='gallery'),
-    path('/contact', views.contact_page, name='contact'),
+    path('gallery', views.gallery_page, name='gallery'),
+    path('contact', views.contact_page, name='contact'),
 ]
